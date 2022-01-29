d7afd7828ec0b6e2811f8089407e1378fab5694c
jinhyungchoi <wlsgudchoi@gmail.com>
2022-01-30 05:51:55 +0900

    feat: 백준 1654번 랜선 자르기

--- BOJ/src/1654.kt
+++ BOJ/src/1654.kt
@@ -1,23 +1,23 @@
 // 랜선 자르기
 
-fun main(){
+fun main() {
     val br = System.`in`.bufferedReader()
-    val (N,K) = br.readLine().split(' ').map { it.toLong() }
+    val (N, K) = br.readLine().split(' ').map { it.toLong() }
     val lans = mutableListOf<Long>()
-    repeat(N.toInt()){
+    repeat(N.toInt()) {
         lans.add(br.readLine().toLong())
     }
     var average = (lans.sum() / K)
     var first = 1L
     var answer = 0L
-    while (first <= average){
+    while (first <= average) {
         val mid = (first + average) / 2
-        val list = lans.map { it/mid }
-        val s = list.sum()
-        if (s < K){
-            average = mid -1
-        } else{
-            first = mid +1
+        lans.sumOf { it / mid }.let {
+            if (it < K) {
+                average = mid - 1
+            } else {
+                first = mid + 1
+            }
         }
         answer = average
     }
